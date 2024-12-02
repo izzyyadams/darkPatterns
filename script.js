77feff4d267c10800a182dfc84b48c4ddebfd677
izzyyadams <ipa2010@nyu.edu>
2024-12-02 08:19:16 -0500

    library2

--- script.js
+++ script.js
@@ -7,10 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
     	opacity: 0, 
     	duration: 1, 
     	stagger: 0.5 });
-    gsap.fromTo(".mainTitle", 
-  		{ rotation: -5 }, 
-  		{ rotation: 5, duration: 0.1, repeat: 5, yoyo: true }
-);
+    gsap.fromTo(".mainTitle", { 
+    	rotation: -5 }, { 
+    		rotation: 5, duration: 0.1, repeat: 5, yoyo: true }
+	);
+	const subtitles = document.querySelectorAll(".subtitle");
+	console.log(subtitles);
+    
+    subtitles.forEach((subtitle) => {
+        gsap.from(subtitle, {
+            opacity: 0,
+            y: -50,
+            duration: 1,
+            scrollTrigger: {
+                trigger: subtitle, 
+                start: "top 80%",  
+                toggleActions: "play none none none", 
+            }
+        });
+    });
+    
 
 
     
